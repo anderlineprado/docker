ef81add7bde0dae56149b89d59c95e41baaa5819
Anderson Prado <anderson@connectcorp.com.br>
2021-01-26 15:36:55 -0300

    insert and display name

--- node/index.js
+++ node/index.js
@@ -1,8 +1,34 @@
 const express = require('express')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
 const app = express();
+app.use(bodyParser.json())
+
+const connection = mysql.createConnection({
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "database"
+})
+
+const createTable = `create table if not exists people (name VARCHAR(255))`
+const createPerson = `insert into people (name) values ('Anderson')`
+
+connection.query(createTable)
+connection.query(createPerson)
 
 app.get('/', (req, res) => {
-	res.send('Sup man!!')
+    const query = `select * from people`
+    connection.query(query, (error, results) => {
+        for(let result of results) {
+            res.send(`
+                <h1>FullCycle Rocks!!</h1>
+                <p>${result["name"]}</p>
+            `)
+        }
+    });
+    connection.end()
 })
 
 app.listen(3000, () => {
